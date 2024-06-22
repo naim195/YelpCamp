ce0874fd3779a9210920e988ee504e6cb1b6dc04
naim195 <119221365+naim195@users.noreply.github.com>
2024-06-23 01:45:39 +0530

    added styles to home,login,register pages

--- models/campgrounds.js
+++ models/campgrounds.js
@@ -13,42 +13,43 @@ ImageSchema.virtual("thumbnail").get(function () {
 
 const opts = { toJSON: { virtuals: true } };
 
-
-const CampgroundSchema = new Schema({
-  title: String,
-  images: [ImageSchema],
-  geometry: {
-    type: {
-      type: String,
-      enum: ["Point"],
-      required: true,
+const CampgroundSchema = new Schema(
+  {
+    title: String,
+    images: [ImageSchema],
+    geometry: {
+      type: {
+        type: String,
+        enum: ["Point"],
+        required: true,
+      },
+      coordinates: {
+        type: [Number],
+        required: true,
+      },
     },
-    coordinates: {
-      type: [Number],
-      required: true,
-    },
-  },
-  price: Number,
-  description: String,
-  location: String,
-  author: {
-    type: Schema.Types.ObjectId,
-    ref: "User",
-  },
-  reviews: [
-    {
+    price: Number,
+    description: String,
+    location: String,
+    author: {
       type: Schema.Types.ObjectId,
-      ref: "Review",
+      ref: "User",
     },
-  ],
-},opts);
+    reviews: [
+      {
+        type: Schema.Types.ObjectId,
+        ref: "Review",
+      },
+    ],
+  },
+  opts,
+);
 
-CampgroundSchema.virtual('properties.popUpMarkup').get(function () {
+CampgroundSchema.virtual("properties.popUpMarkup").get(function () {
   return `
   <strong><a href="/campgrounds/${this._id}">${this.title}</a><strong>
-  <p>${this.description.substring(0, 20)}...</p>`
+  <p>${this.description.substring(0, 20)}...</p>`;
 });
-
 
 CampgroundSchema.post("findOneAndDelete", async function (doc) {
   if (doc) {
